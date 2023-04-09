caf67c8bd88a77c791a9dc66161d13e1d6c6a944
PrettyCoffee <66521452+PrettyCoffee@users.noreply.github.com>
2023-04-09 21:43:55 +0200

    feat: Add multiple milestones

--- public/index.tsx
+++ public/index.tsx
@@ -2,11 +2,25 @@ import { hydrate, prerender as ssr } from "preact-iso"
 
 import { Timer } from "components"
 
+import milestones from "./milestones.json"
+
+const sorted = milestones.sort((a, b) => {
+  const dateA = new Date(a.deadline)
+  const dateB = new Date(b.deadline)
+  return dateA.valueOf() - dateB.valueOf()
+})
+
+const currentMilestone = sorted.find(milestone => {
+  const date = new Date(milestone.deadline)
+  const now = Date.now()
+  return date.valueOf() - now > 0
+})
+
 export const App = () => (
   <div className={"app"}>
     <Timer
-      label="Zeit bis zur Abgabe"
-      endDate={new Date("2023-03-23T00:00:00")}
+      label={currentMilestone?.label ?? ""}
+      endDate={new Date(currentMilestone?.deadline ?? "")}
     />
   </div>
 )
