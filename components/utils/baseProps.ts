be7f58132c15f56ca80cb8748fa3302593d3dca9
PrettyCoffee <jonathan-holz@outlook.com>
2023-04-15 01:43:01 +0200

    Updates

--- components/utils/baseProps.ts
+++ components/utils/baseProps.ts
@@ -1,3 +1,5 @@
+type Child = JSX.Element | string | number | null | undefined
+
 export type PropsWithChildren<T extends object = Record<string, never>> = T & {
-  children: JSX.Element | JSX.Element[]
+  children: Child | Child[]
 }
