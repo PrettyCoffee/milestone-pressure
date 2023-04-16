3768f8e93010f37cf7c2aeca7eedc53aa5e66169
PrettyCoffee <jonathan-holz@outlook.com>
2023-04-16 22:19:16 +0200

    fix(useLocalStorage): Prevent prebuild from failing

--- public/hooks/useLocalStorage.ts
+++ public/hooks/useLocalStorage.ts
@@ -1,40 +1,47 @@
 import { useCallback, useState } from "preact/hooks"
 
-const STORAGE = window.localStorage || {}
+const PREFIX = "miles-"
 
-const parseStorage = <T>(key: string) => {
-  const stringValue = STORAGE.getItem(key)
-  if (stringValue == null) return null
+const STORAGE = {
+  setItem: <T>(key: string, value: T) => {
+    if (typeof window === "undefined") return
+    window.localStorage.setItem(key, JSON.stringify(value))
+  },
 
-  try {
-    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-    return JSON.parse(stringValue) as T
-  } catch {
-    return null
-  }
+  getItem: <T>(key: string) => {
+    if (typeof window === "undefined") return null
+
+    const stringValue = window.localStorage.getItem(key)
+    if (stringValue == null) return null
+
+    try {
+      // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+      return JSON.parse(stringValue) as T
+    } catch {
+      return null
+    }
+  },
 }
 
 const initiateStorage = <T>(key: string, initialValue: T) => {
-  const value = parseStorage<T>(key)
+  const value = STORAGE.getItem<T>(key)
   if (value != null) return value
 
-  STORAGE.setItem(key, JSON.stringify(initialValue))
+  STORAGE.setItem(key, initialValue)
   return initialValue
 }
-
-const prefix = "miles-"
 
 export const useLocalStorage = <T>(
   key: string,
   initialValue: T
 ): [T, (value: T) => void] => {
   const [value, setValue] = useState(
-    initiateStorage(prefix + key, initialValue)
+    initiateStorage(PREFIX + key, initialValue)
   )
 
   const setStorage = useCallback(
     (value: T) => {
-      STORAGE.setItem(prefix + key, JSON.stringify(value))
+      STORAGE.setItem(PREFIX + key, value)
       setValue(value)
     },
     [key]
