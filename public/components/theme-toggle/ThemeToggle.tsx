5be3679cda6a3617717cf23d22c36e2a380c151b
PrettyCoffee <jonathan-holz@outlook.com>
2023-04-18 01:44:27 +0200

    feat(ThemeToggle): Sort themes by lightness

--- public/components/theme-toggle/ThemeToggle.tsx
+++ public/components/theme-toggle/ThemeToggle.tsx
@@ -3,6 +3,37 @@ import { useId } from "preact/hooks"
 import styles from "./theme-toggle.module.css"
 import { ThemeInput } from "./ThemeInput"
 
+const hexToNumber = (hex: string) => Number(`0x${hex}`)
+
+const splitHexColor = ([, ...hex]: string) => ({
+  r: hex[0] + hex[1],
+  g: hex[2] + hex[3],
+  b: hex[4] + hex[5],
+})
+
+const getThemeVarValue = (theme: string, varName: string) => {
+  const span = document.createElement("span")
+  span.className = theme
+  document.body.appendChild(span)
+  return window.getComputedStyle(span).getPropertyValue(varName)
+}
+
+const sortByLightness = (themes: string[]) => {
+  const withWeight = themes.map(theme => {
+    const base = getThemeVarValue(theme, "--base")
+    const { r, g, b } = splitHexColor(base)
+
+    return {
+      theme,
+      weight: hexToNumber(r) + hexToNumber(g) + hexToNumber(b),
+    }
+  })
+
+  return withWeight
+    .sort((a, b) => (a.weight > b.weight ? 1 : -1))
+    .map(({ theme }) => theme)
+}
+
 interface ThemeToggleProps {
   current: string
   options: string[]
@@ -24,7 +55,7 @@ export const ThemeToggle = ({
       </label>
 
       <div aria-labelledby={labelId} className={styles.wrapper}>
-        {options.sort().map(theme => (
+        {sortByLightness(options).map(theme => (
           <ThemeInput
             key={theme}
             theme={theme}
