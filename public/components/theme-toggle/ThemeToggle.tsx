06a3007afa6f6d45bab06263aaf40367ed824d58
PrettyCoffee <jonathan-holz@outlook.com>
2023-04-17 19:50:19 +0200

    feat(ThemeToggle): Sort themes

--- public/components/theme-toggle/ThemeToggle.tsx
+++ public/components/theme-toggle/ThemeToggle.tsx
@@ -8,13 +8,23 @@ interface ThemeToggleProps {
   onChange: (name: string) => void
 }
 
+const sortOptions = (options: string[], current: string) => {
+  const sorted = options.sort((a, b) => {
+    if (a === current) return -1
+    if (b === current) return 1
+    return a < b ? -1 : 1
+  })
+  console.log(sorted)
+  return sorted
+}
+
 export const ThemeToggle = ({
   current,
   options,
   onChange,
 }: ThemeToggleProps) => (
   <div className={styles.wrapper}>
-    {options.map(name => (
+    {sortOptions(options, current).map(name => (
       <button
         key={name}
         className={joinClassNames(styles.button)}
