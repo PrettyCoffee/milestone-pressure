470cbabe11baba4438d528ab26b4545c8717a0bd
PrettyCoffee <jonathan-holz@outlook.com>
2023-04-17 20:21:22 +0200

    feat(ThemeToggle): Highlight current theme

--- public/components/theme-toggle/ThemeToggle.tsx
+++ public/components/theme-toggle/ThemeToggle.tsx
@@ -8,15 +8,16 @@ interface ThemeToggleProps {
   onChange: (name: string) => void
 }
 
-const sortOptions = (options: string[], current: string) => {
-  const sorted = options.sort((a, b) => {
-    if (a === current) return -1
-    if (b === current) return 1
-    return a < b ? -1 : 1
-  })
-  console.log(sorted)
-  return sorted
+interface ThemeBubblesProps {
+  theme: string
 }
+const ThemeBubbles = ({ theme }: ThemeBubblesProps) => (
+  <div className={joinClassNames(styles.bubbles, theme)}>
+    <span />
+    <span />
+    <span />
+  </div>
+)
 
 export const ThemeToggle = ({
   current,
@@ -24,22 +25,28 @@ export const ThemeToggle = ({
   onChange,
 }: ThemeToggleProps) => (
   <div className={styles.wrapper}>
-    {sortOptions(options, current).map(name => (
-      <button
-        key={name}
-        className={joinClassNames(styles.button)}
-        role="radio"
-        aria-checked={current === name}
-        onClick={() => onChange(name)}
-      >
-        <div className={joinClassNames(styles.bubbles, name)}>
-          <span />
-          <span />
-          <span />
-        </div>
+    <div className={styles.themeOption} role="radio" aria-checked>
+      <ThemeBubbles theme={current} />
+      <span className="visually-hidden">
+        Currently selected theme: {current}
+      </span>
+    </div>
 
-        <span className="visually-hidden">Change theme to {name}</span>
-      </button>
-    ))}
+    {options
+      .filter(option => option !== current)
+      .sort()
+      .map(name => (
+        <button
+          key={name}
+          className={joinClassNames(styles.themeOption)}
+          role="radio"
+          aria-checked={current === name}
+          onClick={() => onChange(name)}
+        >
+          <ThemeBubbles theme={name} />
+
+          <span className="visually-hidden">Change theme to {name}</span>
+        </button>
+      ))}
   </div>
 )
